c0f0a036a68d6c2599a06c09e0ca7646e38d768e
crisandolindesmanrumahorbo <crisandolin007@gmail.com>
2025-05-02 20:54:03 +0700

    add migration and seed sql

--- migrations/migrations.sql
+++ migrations/migrations.sql
@@ -1,5 +1,31 @@
 CREATE TABLE users (
-	id SERIAL PRIMARY KEY,
-	username VARCHAR(255) UNIQUE NOT NULL,
-	password TEXT NOT NULL
+	user_id SERIAL PRIMARY KEY,
+	username VARCHAR(50) UNIQUE NOT NULL,
+	password TEXT NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE products (
+  product_id SERIAL PRIMARY KEY,
+  symbol VARCHAR(10) UNIQUE NOT NULL,
+  name VARCHAR(100) UNIQUE NOT NULL,
+  tags VARCHAR(100),
+  last_updated TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE portfolios (
+  portfolio_id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL,
+  product_id INT NOT NULL,
+  avg_price DECIMAL(10,2) NOT NULL,
+  lot INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  FOREIGN KEY (product_id) REFERENCES products(product_id),
+  CONSTRAINT unique_user_product UNIQUE (user_id, product_id)
+);
+
+
+/* TODO 
+account balance
+ */
+
---
+++ migrations/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO products (symbol, name, tags) VALUES ('XBTUSD', 'Bitcoin', 'S,TL');
+INSERT INTO products (symbol, name, tags) VALUES ('ETHUSD', 'Ethereum', 'S');
+INSERT INTO products (symbol, name, tags) VALUES ('SOLUSDT', 'Solana', 'TB');
+INSERT INTO products (symbol, name, tags) VALUES ('DOGEUSDT', 'Dogecoin', 'S,TB');
+INSERT INTO products (symbol, name, tags) VALUES ('SUIUSD', 'Sui', 'TB,TL');
+
